60eba03055d9f012d22355215f819f3dfa6dbe17
Jen Parker <jenparker1975@gmail.com>
2023-12-01 08:22:27 -0500

    Day 1 challenge

---
+++ AdventOfCode2023/Day01.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
+{
+    internal class Day01
+    {
+        public int SolvePartOne()
+        {
+            return solveProblem(true);
+        }
+
+        public int SolvePartTwo()
+        {
+            return solveProblem(false);
+        }
+
+        private int solveProblem(bool part1)
+        {
+            var replacements = getReplacements(part1);
+            string[] lines = File.ReadAllLines("Day01numbers.txt");
+            string matcher = string.Join('|', replacements.Keys);
+            int sum = 0;               
+            foreach (string line in lines)
+            {
+                string first = Regex.Match(line, matcher).Value;
+                var last = Regex.Match(line, matcher, RegexOptions.RightToLeft).Value;
+                sum += int.Parse(replacements[first] + replacements[last]);
+            }
+
+            return sum;
+        }
+
+
+        private Dictionary<string, string> getReplacements(bool part1)
+        {
+            if (part1)
+            {
+                return new Dictionary<string, string>
+                {
+                    { "1", "1" },
+                    { "2", "2" },
+                    { "3", "3" },
+                    { "4", "4" },
+                    { "5", "5" },
+                    { "6", "6" },
+                    { "7", "7" },
+                    { "8", "8" },
+                    { "9", "9" }                 
+                };
+            }
+
+
+            return new Dictionary<string, string>
+            {
+                { "1", "1" },
+                { "2", "2" },
+                { "3", "3" },
+                { "4", "4" },
+                { "5", "5" },
+                { "6", "6" },
+                { "7", "7" },
+                { "8", "8" },
+                { "9", "9" },
+                { "one", "1" },
+                { "two", "2" },
+                { "three", "3" },
+                { "four", "4" },
+                { "five", "5" },
+                { "six", "6" },
+                { "seven", "7" },
+                { "eight", "8" },
+                { "nine", "9" },
+            };
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2023;
+
+Console.WriteLine("Hello, World!");
+Day01 day1 = new Day01();
+var results = day1.SolvePartOne();
+Console.WriteLine($"Part One: {results}");
+
+results = day1.SolvePartTwo();
+Console.WriteLine($"Part One: {results}");
+
+
+
+
+
+
